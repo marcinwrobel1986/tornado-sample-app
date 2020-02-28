8e28d505de1607b613d8de25af230cdb4caed60b
Marcin Wrobel <marcin.wrobel@visma.com>
2020-02-28 14:42:56 +0100

    add python files

---
+++ hello_world/main.py
@@ -0,0 +1,20 @@
+import tornado.ioloop
+import tornado.web
+
+class MainHandler(tornado.web.RequestHandler):
+    async def get(self):
+        self.write("Hello, world")
+
+class HealthCheck(tornado.web.RequestHandler):
+    async def get(self):
+        self.write("OK")
+
+def make_app():
+    return tornado.web.Application([
+        (r"/", MainHandler),
+    ])
+
+if __name__ == "__main__":
+    app = make_app()
+    app.listen(5000)
+    tornado.ioloop.IOLoop.current().start()
